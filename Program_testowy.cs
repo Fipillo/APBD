9495b07375f206a75e2c8b6e737dd6e80f75434f
Filip Szymanski <wafel@Filips-MacBook-Pro.local>
2024-03-14 19:09:30 +0100

    calulating average feature added :)

--- Program_testowy.cs
+++ Program_testowy.cs
@@ -7,3 +7,15 @@ Console.WriteLine("Commit_1 done");
 
 
 Console.WriteLine("zamiast modyfikacji wypusije commit, whatever")
+
+public static double CalculateAverage(int[] numbers)
+{
+    int sum = 0;
+    
+    foreach (int number in numbers)
+    {
+        sum += number;
+    }
+
+    return sum / numbers.Length;
+}
